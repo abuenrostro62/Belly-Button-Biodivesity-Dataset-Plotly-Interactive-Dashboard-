46deeb4f577e131a2cb9637b34d2a5ed51a244c6
Ari Buenrostro <aribuenrostro@Aris-MacBook-Pro-2.local>
2020-04-25 14:15:54 -0500

    first phase

---
+++ static/js/app.js
@@ -0,0 +1,29 @@
+console.log("I am working")
+
+// //Use `d3.json` to fetch the metadata for a sample
+// d3.json("samples.json", function(data) {
+//     //Write the response to the console
+//     console.log(data);
+// });
+
+// // Use D3 to create an event handler
+// d3.selectAll("#selDataset").on("change", getData);
+
+//     function getData() {
+//         // Use D3 to select the dropdown menu
+//         var dropdownMenu = d3.selectAll("#selDataset");
+//         // Assign the dropdown menu item ID to a variable
+//         var dataset = dropdownMenu.property("value");
+//         // Call function to update the chart
+//         updatePlotly(getData);
+
+//         // console.log(dropdownMenuID);
+        
+// }
+
+
+
+    
+
+
+
